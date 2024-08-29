a24e148a50e96e2275268b231f25a5a5c79e79da
boschla1 <bradley.schlather@hobbylobby.com>
2024-08-29 08:44:37 -0500

    to dos added

--- mock-draft-web-app/src/app/home/home.component.ts
+++ mock-draft-web-app/src/app/home/home.component.ts
@@ -12,5 +12,7 @@ export class HomeComponent {
   //To Do: I need to check the way I'm copying objects. Currently using soft copies which could lead to issues in future.
   //To Do: Need to add a simulator that allows the user to be a team of his choice.
   //To Do: Add an option to upgrade the user's account for a small subscription fee.
+  //To Do: Also need to add week by week options to pick who they think will win.
+  //To Do: Also need to allow them to make predictions prior to season start, such as SB winner, playoff teams, and other things of the like.
 
 }
