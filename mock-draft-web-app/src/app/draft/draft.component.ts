9ae92a24ba06c72b0314f825976195eb00fb9e17
boschla1 <bradley.schlather@hobbylobby.com>
2024-09-02 12:17:10 -0500

    trading works

--- mock-draft-web-app/src/app/draft/draft.component.ts
+++ mock-draft-web-app/src/app/draft/draft.component.ts
@@ -20,6 +20,9 @@ export class DraftComponent implements OnInit {
   public draftOrder: string[] = [];
   public imageArr: string[] = [];
   public picks = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+  private tradeTeam1 = '';
+  private tradeTeam2 = '';
+  private tradeTeam1Index = 0;
 
   constructor(private apiService: ApiService, private dialog: MatDialog) { }
 
@@ -44,19 +47,34 @@ export class DraftComponent implements OnInit {
     })
   }
 
-  public openTradeDialog(index: number, teams: Team[] = this.teams) {
-    let dataToPass: TradeDataToDialog = { index, teams }
-    this.dialog.open(TradeDialogComponent, {
-      data: dataToPass
-    }).afterClosed().subscribe(confirmed => {
-      if (confirmed)
-        //To Do: This is where I'll need to get the teams again, which will populate the correct order following the trade
-        console.log('Replacement for getting Teams List');
-    })
+  public trade(index: number) {
+    if (this.tradeTeam1 == '') {
+      this.tradeTeam1 = this.draftOrder[index];
+      this.tradeTeam1Index = index;
+    }
+    else {
+      this.tradeTeam2 = this.draftOrder[index];
+      this.draftOrder[index] = this.tradeTeam1;
+      this.imageArr[index] = `../../assets/${this.tradeTeam1}Logo.gif`;
+      this.draftOrder[this.tradeTeam1Index] = this.tradeTeam2;
+      this.imageArr[this.tradeTeam1Index] = `../../assets/${this.tradeTeam2}Logo.gif`;
+    }
   }
+
+  // public openTradeDialog(index: number, teams: Team[] = this.teams) {
+  //   let dataToPass: TradeDataToDialog = { index, teams }
+  //   this.dialog.open(TradeDialogComponent, {
+  //     data: dataToPass
+  //   }).afterClosed().subscribe(confirmed => {
+  //     if (confirmed)
+  //       //To Do: This is where I'll need to get the teams again, which will populate the correct order following the trade
+  //       console.log('Replacement for getting Teams List');
+  //   })
+  // }
 
   public onSave(): void {
     //Will use proc to set picks for user in database. proc will be called from .NET backend. When onSave() is called, picks will be sent to database for that user
+    //To Do: Ensure I capture all data for trades too
     //To Do: need to prevent player from being added twice when I call onSave()
     //To Do: will send user to account creation screen if user doesn't have an account
   }
